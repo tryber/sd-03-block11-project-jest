2d3c574f0d84bae9153e913b94388d2012728950
tales <talestales@gmail.com>
2020-04-16 19:24:51 -0300

    h

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -27,7 +27,7 @@ const user = {
   name: { first: 'Antônio', last: 'Britto' },
   location: { country: 'Brazil' },
   email: 'tunico@bol.com.br',
-  login: { username: 'tunicao123', password: '1234567890'}
+  login: { username: 'tunicao123', password: '1234567890' }
 }
 
 
@@ -35,14 +35,14 @@ describe('verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
 
   test('verifica se o usuário é o tunico', async () => {
-    return api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
-      expect(user.name.first).toEqual('Antônio');
-      expect(user.name.last).toEqual('Britto');
-      expect(user.location.country).toEqual('Brazil');
-      expect(user.email).toEqual('tunico@bol.com.br');
-      expect(user.login.username).toEqual('tunicao123');
-      expect(user.login.password).toEqual('1234567890');
-    });
+    //return api.fetchURL().then((user) => {
+    expect(user.gender).toEqual('male');
+    expect(user.name.first).toEqual('Antônio');
+    expect(user.name.last).toEqual('Britto');
+    expect(user.location.country).toEqual('Brazil');
+    expect(user.email).toEqual('tunico@bol.com.br');
+    expect(user.login.username).toEqual('tunicao123');
+    expect(user.login.password).toEqual('1234567890');
   });
+});
 });
