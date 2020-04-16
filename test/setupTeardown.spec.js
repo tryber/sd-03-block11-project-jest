199718b6e07d1922bd3e4987a17a606c4d2ee6c3
tales <talestales@gmail.com>
2020-04-16 19:40:00 -0300

    i

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -1,4 +1,4 @@
-const adventure = require('../src/setupTeardown');
+// const adventure = require('../src/setupTeardown');
 /*
 Num universo não tão distante, um grupo de aventureiros da Trybe enfrentam uma série de testes.
 O grupo parte em direção ao sucesso, mas,
@@ -18,22 +18,35 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+const specialists = [{ nome: 'Tunico', classe: 'Mestre dos Magos' },
+                  { nome: 'Cremoso', classe: 'Cavaleiro Negro' },
+                  { nome: 'Gordinho', classe: 'Pirata' },
+                  { nome: 'Lobo Solitário', classe: 'Samurai' },
+                  { nome: 'Ousado', classe: 'Mago' },
+                  { nome: 'Anjo Loiro', classe: 'Clérigo' }];
+
+
+const randomSpecialistIndex = Math.floor(Math.random() * specialists.length);
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
-
+  specialists.splice(randomSpecialistIndex, 1);
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
+  specialists.splice(randomSpecialistIndex, 1);
   test('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
   });
+  specialists.splice(randomSpecialistIndex, 1);
   test('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
   });
+  specialists.splice(randomSpecialistIndex, 1);
   test('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
   });
+  specialists.splice(randomSpecialistIndex, 1);
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
