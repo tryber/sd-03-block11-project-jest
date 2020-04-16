f32c09f6f903257ac338825aa15f838156f2de71
leonardodamasceno8 <leonardodamasceno8@users>
2020-04-16 15:36:59 -0300

    First Commit require number 1 done

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(true).then(resolve => expect(resolve).toEqual('Oi!'));
   });
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    expect.assertions(1);
+    return answerPhone(false).catch(reject => expect(reject).toEqual('Infelizmente não podemos atender...'));
   });
 });
