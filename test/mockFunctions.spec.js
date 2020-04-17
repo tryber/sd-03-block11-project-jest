803d777c4790e30b355b2b8b1be9a165c244ed57
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-17 20:10:24 -0300

    mock

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,8 +1,8 @@
 const mockFunctions = require('../src/mockFunctions');
+jest.mock('../src/mockFunctions');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
-jest.mock('../src/mockFunctions');
 Elas estão no arquivo 'src/mockFunctions.js'.
 Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
 não como estão sendo feitos no arquivo original.
@@ -11,6 +11,11 @@ importados apenas na suite de testes abaixo.
 
 Importante! A correção de código via mock functions não é uma aplicação usual.
 O foco aqui é a utilização de mock functions.
+ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+*/
+
+describe('verifica as funções e os mocks', () => {
+  // Crie suas mock functions aqui
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
@@ -23,12 +28,6 @@ O foco aqui é a utilização de mock functions.
     }
     return fact;
   });
-
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
-
-describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
