5be470a565a71261fe4cc777030400c5fd401588
Hfreitas <hfreitas86@gmail.com>
2020-04-17 17:10:56 -0300

    implementados testes do arquivo mockFunctions.spec.js

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,4 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
+jest.mock('../src/mockFunctions.js');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
@@ -16,6 +17,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
+  mockFunctions.add.mockImplementation((a,b) => a + b);
+  mockFunctions.subtract.mockImplementation((a,b) => a - b);
+  mockFunctions.multiply.mockImplementation((a,b) => a * b);
+  mockFunctions.divide.mockImplementation((a,b) => a / b);
+  mockFunctions.power.mockImplementation((a,b) => Math.pow(a,b));
+  mockFunctions.factorial.mockImplementation(
+    (factorial = number => (number > 1 ? number * factorial(number - 1) : 1)));
+
+
+
+
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
