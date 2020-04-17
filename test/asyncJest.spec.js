e6711397aeef56aea6a747e442af9939ba168632
Marco Barbosa <marco.meireles.b@gmail.com>
2020-04-17 15:17:46 -0300

    Teste assincrono resolvido

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,17 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    return answerPhone(true).then((result) => {
+      expect(result).toEqual('Oi!');
+    });
   });
   test("ocupado", () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
+    return answerPhone(false).catch((error) => {
+      expect(error).toEqual('Infelizmente não podemos atender...');
+    });
   });
 });
