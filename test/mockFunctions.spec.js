9183093bb3bb0c6a43b19a0acdcf88cd1053a16f
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-04-17 20:03:04 -0300

    Conflicts

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,8 +1,8 @@
 const mockFunctions = require('../src/mockFunctions');
-jest.mock('../src/mockFunctions');
 
 /*
 Criamos uma série de funções com eficiência duvidosa.
+jest.mock('../src/mockFunctions');
 Elas estão no arquivo 'src/mockFunctions.js'.
 Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
 não como estão sendo feitos no arquivo original.
@@ -12,23 +12,23 @@ importados apenas na suite de testes abaixo.
 Importante! A correção de código via mock functions não é uma aplicação usual.
 O foco aqui é a utilização de mock functions.
 
+mockFunctions.add.mockImplementation((a, b) => a + b);
+mockFunctions.subtract.mockImplementation((a, b) => a - b);
+mockFunctions.multiply.mockImplementation((a, b) => a * b);
+mockFunctions.divide.mockImplementation((a, b) => a / b);
+mockFunctions.power.mockImplementation((a, b) => a ** b);
+mockFunctions.factorial.mockImplementation((a) => {
+let fact = 1;
+for (let i = 2; i <= a; i += 1) {
+fact = fact * i;
+}
+return fact;
+});
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
-  mockFunctions.add.mockImplementation((a, b) => a + b);
-  mockFunctions.subtract.mockImplementation((a, b) => a - b);
-  mockFunctions.multiply.mockImplementation((a, b) => a * b);
-  mockFunctions.divide.mockImplementation((a, b) => a / b);
-  mockFunctions.power.mockImplementation((a, b) => a ** b);
-  mockFunctions.factorial.mockImplementation((a) => {
-    let fact = 1;
-    for (let i = 2; i <= a; i += 1) {
-      fact = fact * i;
-    }
-    return fact;
-  });
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
