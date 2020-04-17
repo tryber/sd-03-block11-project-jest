402c60d17e0ebea7e7e3136a1f0dbed6124cf21a
Cardoso <lsc.cardosoo@gmail.com>
2020-04-17 17:02:54 -0300

    Atualizar requisito

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -19,9 +19,9 @@ describe('verifica as funções e os mocks', () => {
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
   mockFunctions.multiply.mockImplementation((a, b) => a * b);
-  divide.mockImplementation((a, b) => a / b);
-  power.mockImplementation((a, b) => Math.pow(a, b));
-  factorial.mockImplementation((a) => {
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.power.mockImplementation((a, b) => Math.pow(a, b));
+  mockFunctions.factorial.mockImplementation((a) => {
     let factor = 1;
     for (let i = 1; i <= a; i += 1) {
       factor *= i;
