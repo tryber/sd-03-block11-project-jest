56f285c3917db9d29715ca5b9edea93c5a7cb4d8
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-04-17 16:32:31 -0300

    conflict

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,7 +15,6 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
   add.mockImplementation((a, b) => a + b);
   subtract.mockImplementation((a, b) => a - b);
   multiply.mockImplementation((a, b) => a * b);
