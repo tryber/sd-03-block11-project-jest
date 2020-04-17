7c5563f02d28311625d5d42afca461164b0dd7c9
Fernando Luckesi <fernando.luckesi@94gmail.com>
2020-04-17 13:49:46 -0300

    Requisito 2 em produção

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -18,6 +18,7 @@ describe('verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
   
   test('testa função add', () => {
+    add = jest.fn().mockImplementation((a, b) => a + b);
     expect(add(1, 2)).toEqual(3);
     expect(add(8, 37)).toEqual(45);
     expect(add(-11, 25)).toEqual(14);
@@ -25,6 +26,7 @@ describe('verifica as funções e os mocks', () => {
     expect(add(7, 26)).toEqual(33);
   });
   test('testa função subtract', () => {
+    subtract = jest.fn().mockImplementation((a, b) => a - b);
     expect(subtract(899, 35)).toEqual(864);
     expect(subtract(-17, 333)).toEqual(-350);
     expect(subtract(45, 97)).toEqual(-52);
@@ -32,6 +34,7 @@ describe('verifica as funções e os mocks', () => {
     expect(subtract(-133, -29)).toEqual(-104);
   });
   test('testa função multiply', () => {
+    multiply = jest.fn().mockImplementation((a, b) => a * b);
     expect(multiply(1, 2)).toEqual(2);
     expect(multiply(0, 5)).toEqual(0);
     expect(multiply(-4, 9)).toEqual(-36);
@@ -39,6 +42,7 @@ describe('verifica as funções e os mocks', () => {
     expect(multiply(19, 23)).toEqual(437);
   });
   test('testa função divide', () => {
+    divide = jest.fn().mockImplementation((a, b) => a / b);
     expect(divide(169, 13)).toEqual(13);
     expect(divide(-1900, 5)).toEqual(-380);
     expect(divide(42, 7)).toEqual(6);
@@ -46,6 +50,7 @@ describe('verifica as funções e os mocks', () => {
     expect(divide(1331, 11)).toEqual(121);
   });
   test('testa função power', () => {
+    power = jest.fn().mockImplementation((a, b) => Math.pow(a, b));
     expect(power(10, 2)).toEqual(100);
     expect(power(2, 10)).toEqual(1024);
     expect(power(5, 5)).toEqual(3125);
@@ -53,6 +58,13 @@ describe('verifica as funções e os mocks', () => {
     expect(power(0, 0)).toEqual(1);
   });
   test('testa função factorial', () => {
+    factorial = jest.fn().mockImplementation((number) => {
+      let total = 1;
+      for (let i = 2; i <= number; i += 1) {
+        total *= i
+      }
+      return total;
+    })
     expect(factorial(5)).toEqual(120);
     expect(factorial(10)).toEqual(3628800);
     expect(factorial(3)).toEqual(6);
