31d33ce17e0a8a14058129aba150e634cea0374d
renan.andreolle <renandreolle@hotmail.com>
2020-04-17 16:57:17 -0300

    Agora vai?

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe("o retorno do telefonema", () => {
   test("atende", () => {
-    assert.fail();
+    return expect(answerPhone('Oi')).resolves.toEqual('Oi!');
     // Insira seu teste assíncrono aqui
   });
   test("ocupado", () => {
-    assert.fail();
+    return expect(answerPhone()).rejects.toEqual('Infelizmente não podemos atender...');
     // Insira seu teste assíncrono aqui
   });
 });
