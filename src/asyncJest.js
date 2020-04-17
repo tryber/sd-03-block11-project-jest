bab3bde41946aabc22828dd73e87bdb9a1c8dc1a
wscatao <wscatao@gmail.com>
2020-04-17 09:43:18 -0300

    chama no teste

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -3,3 +3,13 @@ const answerPhone = answer => new Promise(function (resolve, reject) {
   setTimeout(answer ? resolve('Oi!') : reject('Infelizmente não podemos atender...'), 3000);
 });
 module.exports = answerPhone;
+
+async function teste() {
+  try {
+  const teste = await answerPhone(false)
+  } catch (error) {
+    expect(error).toBe('Infelizmente não podemos atender...')
+  }
+}
+
+teste();
