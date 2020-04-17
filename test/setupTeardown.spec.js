0fac4c7e118d77450caf1f6e09eab050d5ae6648
fabemiliano <fab.emiliano@gmail.com>
2020-04-17 15:06:12 -0300

    outros requisitos

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -18,6 +18,10 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+//adventure.randomAttack()
+
+beforeEach(() => adventure.randomAttack())
+afterEach(() => console.log(adventure.specialists.map(el =>el.nome)))
 
 describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
