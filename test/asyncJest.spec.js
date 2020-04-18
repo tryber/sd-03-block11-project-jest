51a2e8d4a11bc9b4dd32b1d60dfd27db32c32f52
mirellaalves <mirella.alves@gmail.com>
2020-04-18 20:24:52 -0300

    Working at requisite 1.

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,24 +11,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  const req1 = jest.spyOn( answerPhone, "answerPhone");
-  afterEach(req1.mockReset);
 
-  test("atende", async () => {
-    req1.mockResolvedValue('Oi!');
-
-    req1();
-    expect(req1).toHaveBeenCalled();
-    expect(req1).toHaveBeenCalledTimes(1);
-    expect(req1()).resolves.toBe('Oi!');
-    expect(req1).toHaveBeenCalledTimes(2);
+  test("atende", () => {
+    expect.assertions(1);
+    return expect(answerPhone()).resolves.toBe('Oi!');
   });
 
   test("ocupado", () => {
-    req1.mockRejectedValue('Infelizmente não podemos atender...');
-  
-    expect(req1).toHaveBeenCalledTimes(0);
-    expect(req1()).rejects.toMatch('Infelizmente não podemos atender...');
-    expect(req1).toHaveBeenCalledTimes(1);
+    expect.assertions(1);
+    return expect(answerPhone()).rejects.toMatch('Infelizmente não podemos atender...');
   });
 });
