330976b8020afc6f4cd247a9d28518702447261a
mirellaalves <mirella.alves@gmail.com>
2020-04-18 18:26:49 -0300

    Inicial commit.

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,12 +11,24 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe("o retorno do telefonema", () => {
-  test("atende", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+  const req1 = jest.spyOn( answerPhone, "answerPhone");
+  afterEach(req1.mockReset);
+
+  test("atende", async () => {
+    req1.mockResolvedValue('Oi!');
+
+    req1();
+    expect(req1).toHaveBeenCalled();
+    expect(req1).toHaveBeenCalledTimes(1);
+    expect(req1()).resolves.toBe('Oi!');
+    expect(req1).toHaveBeenCalledTimes(2);
   });
+
   test("ocupado", () => {
-    assert.fail();
-    // Insira seu teste assíncrono aqui
+    req1.mockRejectedValue('Infelizmente não podemos atender...');
+  
+    expect(req1).toHaveBeenCalledTimes(0);
+    expect(req1()).rejects.toMatch('Infelizmente não podemos atender...');
+    expect(req1).toHaveBeenCalledTimes(1);
   });
 });
