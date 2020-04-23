aa8e20581d216bf2841d14d20845898cf6b29a91
tamagoshii <estelaap_martins@hotmail.com>
2020-04-23 19:19:03 -0300

    Implementando requisito 2º

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,5 +1,5 @@
 const mockFunctions = require('../src/mockFunctions');
-jest.mock('../src/mockFunctions');
+
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -15,7 +15,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('verifica as funções e os mocks', () => {
-  mockFunctions.add.mockTest((a, b) => a + b);
+  mockFunctions.add = jest.fn.mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn.mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn.mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn.mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn.mockImplementation((a, b) => Matgith.pow(a, b));
+  mockFunctions.factorial = jest.fn.mockImplementation((a)=> (a === 0 || a === 1)? 1 :a * mockFuncitions.factorial(a - 1));
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
