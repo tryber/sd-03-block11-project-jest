61515492e62afc4f8bcda4fdd9692162f4534502
Luma Arruda <luma.oarruda@gmail.com>
2020-05-24 20:33:01 -0300

    sinais trocados da funçao mockFunctions

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -19,8 +19,8 @@ jest.mock('../src/mockFunctions');
 describe('verifica as funções e os mocks', () => {
   mockFunctions.add.mockImplementation((a, b) => a + b);
   mockFunctions.subtract.mockImplementation((a, b) => a - b);
-  mockFunctions.multiply.mockImplementation((a, b) => a / b);
-  mockFunctions.divide.mockImplementation((a, b) => a * b);
+  mockFunctions.multiply.mockImplementation((a, b) => a * b);
+  mockFunctions.divide.mockImplementation((a, b) => a / b);
   mockFunctions.power.mockImplementation((a, b) => a**b);
   mockFunctions.factorial.mockImplementation((a) => {
     return a === 0 ? 1 : a * mockFunctions.factorial(a - 1);
